94d27927700f45450352ec3c291619b4b849cf11
miquel <miquel@projectexinoxano.cat>
2026-05-07 10:41:45 +0200

    fix: add lazyConnect to Redis to prevent build failures

--- lib/services/redis.ts
+++ lib/services/redis.ts
@@ -4,6 +4,7 @@ const redisUrl = process.env.REDIS_URL || 'redis://localhost:6379';
 
 const redis = new Redis(redisUrl, {
   maxRetriesPerRequest: null,
+  lazyConnect: true,
 });
 
 export default redis;
